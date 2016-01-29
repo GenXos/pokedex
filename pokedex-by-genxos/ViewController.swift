ac82c21c17ad864456a5b9335978457b23d38a1b
Todd Fields <toddwfields@gmail.com>
2016-01-29 17:17:49 -0600

    Initial Commit

---
+++ pokedex-by-genxos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-genxos
+//
+//  Created by Todd Fields on 2016-01-29.
+//  Copyright © 2016 Todd Fields. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
